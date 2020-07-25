ab5dca3e9a42968858270f3c18dfb415dd9c28ea
Elliot Liu <aisnote@gmail.com>
2020-07-25 16:29:14 -0700

    refactoring

--- src/components/colorpicker.tsx
+++ src/components/colorpicker.tsx
@@ -6,57 +6,84 @@ interface Props {
     onColorUpdated: (color: Color) => void;
 }
 
+interface PropsColorSlider {
+    value: number;
+    onValueUpdated: (newValue: number) => void;
+}
+
+const ColorSliderComponent = (props: PropsColorSlider) => {
+    return (
+        <div>
+            <input 
+                 type="range"
+                 min="0"
+                 max="255"
+                 value={props.value}
+                 onChange={event => props.onValueUpdated(+event.target.value)}
+            />
+        </div>
+    );
+}
+
+const updateColor = (props: Props, colorId: keyof Color) => (value) => {
+    props.onColorUpdated ({
+        ...props.color,
+        [colorId]: value
+    })
+}
+
+
 export const ColorPicker = (props: Props) => (
-    <div>
-        <input
-            type = "range"
-            min = "0"
-            max = "255"
-            value = {props.color.red}
-            onChange={event => 
-                props.onColorUpdated({
-                    red: +event.target.value,
-                    green: props.color.green,
-                    blue: props.color.blue
-                })
-            }
-
+    <>
+        {Object.keys(props.color).map((field: keyof Color) => (
+            <ColorSliderComponent
+                key={field}
+                value={props.color[field]}
+                onValueUpdated={updateColor(props, field)}
         />
-        {props.color.red}
-        
-        <br />
-        <input
-            type = "range"
-            min = "0"
-            max = "255"
-            value = {props.color.green}
-            onChange={event => 
-                props.onColorUpdated({
-                    red: props.color.red,
-                    green: +event.target.value,
-                    blue: props.color.blue
-                })
-            }
-
-        />
-        {props.color.green}
-
-        <br />
-        <input
-            type = "range"
-            min = "0"
-            max = "255"
-            value = {props.color.blue}
-            onChange={event => 
-                props.onColorUpdated({
-                    red: props.color.red,
-                    green: props.color.green,
-                    blue: +event.target.value
-                })
-            }
-
-        />
-        {props.color.blue}
-
-    </div>
+        ))}
+    </>
 )
+
+// export const ColorPicker = (props: Props) => (
+//     <div>
+//        <ColorSliderComponent
+//             value = {props.color.red}
+//             onValueUpdated={updateColor(props, 'red')}
+//             // onValueUpdated = {(value) => props.onColorUpdated(
+//             //     {
+//             //         red: value,
+//             //         green: props.color.green,
+//             //         blue: props.color.blue
+//             //     }
+//             // )}
+//         />
+        
+//         <br />
+//         <ColorSliderComponent
+//             value = {props.color.green}
+//             onValueUpdated={updateColor(props, 'green')}
+//             // onValueUpdated = {(value) => props.onColorUpdated(
+//             //     {
+//             //         red: props.color.red,
+//             //         green: value,
+//             //         blue: props.color.blue
+//             //     }
+//             // )}
+//         />
+
+//         <br />
+//         <ColorSliderComponent
+//             value = {props.color.blue}
+//             onValueUpdated={updateColor(props, 'blue')}
+//             // onValueUpdated = {(value) => props.onColorUpdated(
+//             //     {
+//             //         red: props.color.red,
+//             //         green: props.color.green,
+//             //         blue: value
+//             //     }
+//             // )}
+//         />
+
+//     </div>
+// )
