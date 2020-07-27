edd6fdd24c6d6b0246c6b9cbea163943de4f96c1
Elliot Liu <aisnote@gmail.com>
2020-07-27 14:42:29 -0700

    for mock rest api

--- src/api/memberApi.ts
+++ src/api/memberApi.ts
@@ -1,21 +1,41 @@
 import { MemberEntity } from "../model/member"
 
+import Axios, { AxiosResponse } from 'axios';
+
+const gitHubURL = 'https://api.github.com';
+const gitHubMembersUrl = `${gitHubURL}/orgs/lemoncode/members`;
+
 export const getMembersCollection = (): Promise<MemberEntity[]> => {
     const promise = new Promise<MemberEntity[]>((resolve, reject) => {
-        setTimeout(() => resolve(
-            [
-                {
-                    id: 1475912,
-                    login: "brauliodiez",
-                    avatar_url: "https://avatars.githubusercontent.com/u/1457912?v=3"
-                },
-                {
-                    id: 4374977,
-                    login: "Nasdan",
-                    avatar_url: "https://avatars.githubusercontent.com/u/4374977?v=3"
-                }
-            ]), 500);
+
+        try {
+            Axios.get<MemberEntity[]>(gitHubMembersUrl)
+            .then(response => resolve(mapMemberListApiToModel(response)))
+        } catch (error) {
+            reject(error)
+        }
+
+        // setTimeout(() => resolve(
+        //     [
+        //         {
+        //             id: 1475912,
+        //             login: "brauliodiez",
+        //             avatar_url: "https://avatars.githubusercontent.com/u/1457912?v=3"
+        //         },
+        //         {
+        //             id: 4374977,
+        //             login: "Nasdan",
+        //             avatar_url: "https://avatars.githubusercontent.com/u/4374977?v=3"
+        //         }
+        //     ]), 500);
     });
 
     return promise;
 }
+
+const mapMemberListApiToModel = ({data}: AxiosResponse<any[]>): MemberEntity[] => 
+data.map(getHubMember => ({
+    id: getHubMember.id,
+    login: getHubMember.login,
+    avatar_url: getHubMember.avatar_url
+}));
